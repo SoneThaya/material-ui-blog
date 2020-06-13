2df00fba48b243d4ba357a24cddbc0a9fcf218c7
Sone <sthaya@gmail.com>
2020-06-13 16:28:24 -0700

    created hero picture section and styled

--- src/App.js
+++ src/App.js
@@ -4,10 +4,24 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
     backgroundColor: '#ffffff'
+  },
+  hero: {
+    backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('https://images.unsplash.com/photo-1454372182658-c712e4c5a1db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')`,
+    height: '500px',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: '#ffffff',
+    fontSize: '4rem'
   }
 }));
 
@@ -24,6 +38,9 @@ function App() {
           </Typography>
         </Toolbar>
       </AppBar>
+      <Box className={classes.hero}>
+        <Box>React Blog</Box>
+      </Box>
     </div>
   );
 }
