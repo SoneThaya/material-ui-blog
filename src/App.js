3e7d84257d058eb7f127449f881a9e0a6abb8487
Sone <sthaya@gmail.com>
2020-06-13 16:44:43 -0700

    created article card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import './App.css';
+
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
@@ -22,6 +29,19 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     color: '#ffffff',
     fontSize: '4rem'
+  },
+  blogsContainer: {
+    paddingTop: theme.spacing(3)
+  },
+  blogTitle: {
+    fontWeight: 800,
+    paddingBottom: theme.spacing(3)
+  },
+  card: {
+    maxWidth: '100%',
+  },
+  media: {
+    height: 240
   }
 }));
 
@@ -41,6 +61,47 @@ function App() {
       <Box className={classes.hero}>
         <Box>React Blog</Box>
       </Box>
+      <Container maxWidth='lg' className={classes.blogsContainer}>
+        <Typography variant="h4" className={classes.blogTitle}>
+          Articles
+        </Typography>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} md={4}>
+            
+            <Card className={classes.card}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+                title="Contemplative Reptile"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  React useContext
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                  across all continents except Antarctica
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              
+            </CardActions>
+          </Card>
+            
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
