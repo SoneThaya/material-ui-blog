f2b2593f57a58cdf14b70b670491d553690dffad
Sone <sthaya@gmail.com>
2020-06-13 17:15:10 -0700

    created pagination bar at the bottom project completed.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import { Avatar } from '@material-ui/core';
 import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import Pagination from '@material-ui/lab/Pagination';
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
@@ -30,7 +31,11 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'center',
     alignItems: 'center',
     color: '#ffffff',
-    fontSize: '4rem'
+    fontSize: '4rem',
+    [theme.breakpoints.down('sm')]: {
+      height: 300,
+      fontSize: '3em'
+    }
   },
   blogsContainer: {
     paddingTop: theme.spacing(3)
@@ -52,6 +57,10 @@ const useStyles = makeStyles((theme) => ({
   },
   author: {
     display: 'flex'
+  },
+  paginationContainer: {
+    display: 'flex',
+    justifyContent: 'center'
   }
 }));
 
@@ -116,14 +125,122 @@ function App() {
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
             
-          </Grid>
-          <Grid item xs={12} sm={6} md={4}>
+          <Card className={classes.card}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image="https://images.unsplash.com/photo-1500322969630-a26ab6eb64cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                React Router
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions className={classes.cardActions}>
+              <Box className={classes.author}>
+                <Avatar src='https://images.unsplash.com/photo-1592004604827-63bab259629f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80' />
+                <Box ml={2}>
+                  <Typography variant="subtitle2" component="p">
+                    Guy Clemons
+                  </Typography>
+                  <Typography variant="subtitle2" color='textSecondary' component="p">
+                    May 14, 2020
+                  </Typography>
+                </Box>
+              </Box>
+              <Box>
+                <BookmarkBorderIcon />
+              </Box>
+          </CardActions>
+            </Card>
             
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
+            
+          <Card className={classes.card}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image="https://images.unsplash.com/photo-1521747116042-5a810fda9664?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                React useState
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions className={classes.cardActions}>
+              <Box className={classes.author}>
+                <Avatar src='https://images.unsplash.com/photo-1592031153694-4fb2045a9138?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80' />
+                <Box ml={2}>
+                  <Typography variant="subtitle2" component="p">
+                    Guy Clemons
+                  </Typography>
+                  <Typography variant="subtitle2" color='textSecondary' component="p">
+                    May 14, 2020
+                  </Typography>
+                </Box>
+              </Box>
+              <Box>
+                <BookmarkBorderIcon />
+              </Box>
+          </CardActions>
+            </Card>
+            
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            
+          <Card className={classes.card}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image="https://images.unsplash.com/photo-1414390579679-7d6cc7b20436?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1699&q=80"
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                React useEffect
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions className={classes.cardActions}>
+              <Box className={classes.author}>
+                <Avatar src='https://images.unsplash.com/photo-1592028545328-7ff4385e450f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80' />
+                <Box ml={2}>
+                  <Typography variant="subtitle2" component="p">
+                    Guy Clemons
+                  </Typography>
+                  <Typography variant="subtitle2" color='textSecondary' component="p">
+                    May 14, 2020
+                  </Typography>
+                </Box>
+              </Box>
+              <Box>
+                <BookmarkBorderIcon />
+              </Box>
+          </CardActions>
+            </Card>
             
           </Grid>
         </Grid>
+        <Box my={4} className={classes.paginationContainer}>
+          <Pagination count={10} />
+        </Box>
       </Container>
     </div>
   );
